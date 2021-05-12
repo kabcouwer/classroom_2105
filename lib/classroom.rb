7fa2fd464bfb01c3e18dd7d5b6c18585b0d84f53
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-05-12 15:36:17 -0600

    Initial commit

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,38 @@
 class Classroom
+  attr_reader :subject,
+              :capacity,
+              :students,
+              :add_student,
+              :yell_at_students,
+              :over_capacity,
+              :kick_out
+
+  def initialize(subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+    @yell_at_students = []
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def yell_at_students
+    students.each do |name|
+      @yell_at_students << name.upcase
+    end
+    return @yell_at_students
+  end
+
+  def over_capacity
+    if students.count <= capacity
+      return false
+    else
+      return true
+    end
+  end
+
+  def kick_out
+
 end
